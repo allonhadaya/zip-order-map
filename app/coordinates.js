3afd53069df04a468d6106b94968166a9275a259
Allon Hadaya <self@allon.nyc>
2017-08-18 22:12:29 -0700

    Add missing dependency. Convert from fetch based streaming with polyfills to old-school xhr progress, offset tracking, and string parsing.

--- app/coordinates.js
+++ app/coordinates.js
@@ -1,34 +1,35 @@
-// polyfill window.ReadableStream
-const { ReadableStream } = require("web-streams-polyfill");
-window.ReadableStream = ReadableStream;
-
-// a streaming fetch interface
-const fetchStream = require('fetch-readablestream')
-
-// parses an incoming response stream as line delimited json documents
-const jsonlStream = require('can-ndjson-stream');
-
-// abstract incoming coordinates as a reactive observable
 const { Observable } = require('rx-lite');
 
 module.exports = Observable.create(observer => {
 
-  fetchStream('/coordinates.jsonl')
-    .then(({ body }) => jsonlStream(body))
-    .then(coordinateStream => {
+  let offset = 0;
 
-      const reader = coordinateStream.getReader();
+  function onProgress(e) {
 
-      // consume all reader values and publish them to the observer
-      reader.read().then(function read({ value, done }) {
-        if (done) {
-          observer.completed();
-        } else {
-          observer.next(value);
-          // continue
-          reader.read().then(read);
-        }
-      });
+    while (true) {
+      const end = this.responseText.indexOf("\n", offset);
+      if (end === -1) break;
+      const line = this.responseText.substring(offset, end);
+      observer.next(JSON.parse(line));
+      offset = end + 1;
+    }
 
-    });
+  }
+
+  function onComplete(e) {
+    const lastLine = this.responseText.substring(offset);
+    observer.next(JSON.parse(lastLine));
+    observer.completed();
+  }
+
+  var request = new XMLHttpRequest();
+
+  request.addEventListener("progress", onProgress);
+  request.addEventListener("load", e => onComplete);
+  request.addEventListener("error", e => observer.error());
+  request.addEventListener("abort", e => observer.cancel());
+
+  request.open("GET", "/coordinates.jsonl");
+  request.send();
+
 });
